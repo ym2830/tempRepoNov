71985a366412c123888202ddddbbf36539ef6958
ym2830 <18604897778@163.com>
2021-12-08 16:42:08 -0500

    Create BondPrice_File.py

---
+++ BondPrice_File.py
@@ -0,0 +1,19 @@
+
+def getBondPrice(y, face, couponRate, m, ppy=1):
+    pvcfsum = 0
+    cf = face * couponRate / ppy
+    for t in range(1,m*ppy+1):
+        pv = (1+y/ppy)**-t
+        pvcf = pv * cf
+        pvcfsum += pvcf
+    bondPrice = pvcfsum + face * pv
+    
+    return(bondPrice)
+
+
+# Test values
+
+y = 0.03
+face = 2000000
+couponRate = 0.04
+m = 10
