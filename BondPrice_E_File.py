2833c0a437fac9feb2981aeb8fe7e8144ae8c947
ym2830 <18604897778@163.com>
2021-12-08 16:41:43 -0500

    Create BondPrice_E_File.py

---
+++ BondPrice_E_File.py
@@ -0,0 +1,19 @@
+
+def getBondPrice_E(face, couponRate, m, yc):
+    coupon = face * couponRate
+    pvcfsum = 0
+    for t,x in enumerate(yc):
+        pv = (1+x)**-(t+1)
+        pvcf = coupon * pv
+        pvcfsum += pvcf
+    bondPrice = pvcfsum + face * pv
+    return(bondPrice)
+
+
+
+# Test values
+
+yc = [.010,.015,.020,.025,.030]
+face = 2000000
+couponRate = .04
+m = 5
